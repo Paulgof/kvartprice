b4e45631455560122eb8a53576920b6da7bb2cd4
Paul Gof <paulgof@outlook.com>
2021-06-01 22:41:01 +0300

    Fix district problem, add page skip and add residental_complex column

--- site_parsers/cian-parser.py
+++ site_parsers/cian-parser.py
@@ -1,7 +1,6 @@
 import csv
 import datetime
 import re
-from itertools import product
 
 import requests
 from bs4 import BeautifulSoup
@@ -75,14 +74,17 @@ def parse_offer(offer_soup):
     floor = flat_desc_dict.get('Этаж')
     flat_floor, total_floors = map(int, floor.split(' из '))
 
+    residental_complex = offer_soup.select_one('div[data-name="Parent"] a[data-name="Link"]')
+    residental_complex_value = residental_complex.get_text()[2:] if residental_complex else ''
+
     geo_info: str = offer_soup.select_one('div[data-name="Geo"] span[itemprop="name"]')['content']
     geo_info_split = geo_info.split(', ')
-    area_value = geo_info_split[2]
-    district_main = geo_info_split[3]
-    district_local = geo_info_split[4] if len(geo_info_split) > 6 else ''
+    district_value = geo_info_split[2]
+    microdistrict_main = geo_info_split[3] if 'ул.' not in geo_info_split[3] else ''
+    microdistrict_local = geo_info_split[4] if len(geo_info_split) > 6 else ''
     flat_number = geo_info_split[-1]
     street = geo_info_split[-2]
-    if 'ул' in flat_number or not any(map(str.isdigit, flat_number)):
+    if 'ул.' in flat_number or not any(map(str.isdigit, flat_number)):
         street, flat_number = flat_number, ''
 
     flat_main_info = offer_soup.select('li[data-name="AdditionalFeatureItem"]')
@@ -107,11 +109,12 @@ def parse_offer(offer_soup):
         'live_square': live_square_value,
         'kitchen_square': kitchen_square_value,
         'floor': flat_floor, 'total_floors': total_floors,
-        'area': area_value,
+        'residental_complex': residental_complex_value,
+        'district': district_value,
         'flat_number': flat_number,
         'street': street,
-        'district_main': district_main,
-        'district_local': district_local,
+        'microdistrict_main': microdistrict_main,
+        'microdistrict_local': microdistrict_local,
         'description': description_value,
         'flat_type': dict_main_info.get('Тип жилья', ''),
         'toilet': dict_main_info.get('Санузел', ''),
@@ -128,7 +131,7 @@ def parse_offer(offer_soup):
 
 
 def scrape_page(url):
-    response = session.get(url)
+    response = session.get(url, allow_redirects=False)
     response.raise_for_status()
     if response.status_code // 100 == 3:
         raise NoMorePagesException()
@@ -142,80 +145,87 @@ def scrape_page(url):
 def init_parsing(file_name):
     timer_start = datetime.datetime.now()
     flats_hashes = set()
-    for room_type, page_number in product(ROOM_TYPES.keys(), range(1, MAX_PAGE + 1)):
-        page_timer_start = datetime.datetime.now()
-        offers_data = {}
-        try:
-            page_offers_links = scrape_page(BASE_URL.format(page=page_number, room_type=room_type))
-            print('Got offers on page {}, room_type {}'.format(page_number, room_type))
-        except NoMorePagesException:
-            print('[NMP]: Skip page {} for room_type {}'.format(page_number, room_type))
-            continue
-        except HTTPError as http_error:
-            print("[HTTP]: Couldn't scrape page.", http_error)
-            continue
-        except Exception as e:
-            print('[DNG]: Unresolved exception. Continue scraping pages.', e)
-            continue
-
-        for offer_link in page_offers_links:
-            offer_id = int(re.search(r'flat/(\d+)/', offer_link)[1])
-            print('Scraping offer on url', offer_link)
+    for room_type in ROOM_TYPES.keys():
+        for page_number in range(1, MAX_PAGE):
+            page_timer_start = datetime.datetime.now()
+            offers_data = {}
             try:
-                offers_data[offer_id] = {  # todo: use asyncio get
-                    'link': offer_link,
-                    'room_type': room_type,
-                    'soup': scrape_offer(offer_link)
-                }
+                page_offers_links = scrape_page(BASE_URL.format(page=page_number, room_type=room_type))
+                print('Got offers on page {}, room_type {}'.format(page_number, room_type))
+            except NoMorePagesException:
+                print('[NMP]: Skip page {} for room_type {}'.format(page_number, room_type))
+                break
             except HTTPError as http_error:
-                print("[HTTP]: Couldn't scrape offer.", http_error)
+                print("[HTTP]: Couldn't scrape page.", http_error)
                 continue
             except Exception as e:
-                print('[DNG]: Unresolved exception. Continue scraping offers.', e)
+                print('[DNG]: Unresolved exception. Continue scraping pages.', e)
                 continue
 
-        page_timer_end = datetime.datetime.now()
-        print('[I] Scraping done for page {}, room_type {} in {} seconds. Start parsing...'.format(
-            page_number,
-            room_type,
-            (page_timer_end - page_timer_start).seconds
-        ))
+            for offer_link in page_offers_links:  # type: str
+                if CITY['name'] not in offer_link:
+                    print('[DCO]: Offer {} is not for {}'.format(offer_link, CITY['name']))
+                    continue
 
-        for offer_id, offer_data in offers_data.items():
-            try:
-                flat_info = parse_offer(offer_data['soup'])
-            except Exception as e:
-                print('[DNG] Exception during parsing. ', e)
-                continue
+                offer_id = int(re.search(r'flat/(\d+)/', offer_link)[1])
+                print('Scraping offer on url', offer_link)
+                try:
+                    offers_data[offer_id] = {
+                        'link': offer_link,
+                        'room_type': room_type,
+                        'soup': scrape_offer(offer_link)
+                    }
+                except HTTPError as http_error:
+                    print("[HTTP]: Couldn't scrape offer.", http_error)
+                    continue
+                except Exception as e:
+                    print('[DNG]: Unresolved exception. Continue scraping offers.', e)
+                    continue
 
-            flat_hash = '{}{}{}{}{}{}'.format(
-                flat_info['total_square'],
-                offer_data['room_type'],
-                flat_info['floor'],
-                flat_info['district_main'],
-                flat_info['street'],
-                flat_info['flat_number']
-            )
-            if flat_hash in flats_hashes:
-                print('[DOUBLE] Skip offer saving')
-                continue
+            page_timer_end = datetime.datetime.now()
+            print('[I] Scraping done for page {}, room_type {} in {} seconds. Start parsing...'.format(
+                page_number,
+                room_type,
+                (page_timer_end - page_timer_start).seconds
+            ))
 
-            flats_hashes.add(flat_hash)
+            for offer_id, offer_data in offers_data.items():
+                try:
+                    flat_info = parse_offer(offer_data['soup'])
+                except Exception as e:
+                    print('[DNG] Exception during parsing. ', e)
+                    continue
 
-            with open(file_name, 'a', newline='', encoding='utf-8') as csv_file:
-                csv_writer = csv.writer(csv_file, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                csv_writer.writerow([
-                    offer_id, offer_data['link'], flat_info['price'], ROOM_TYPES[offer_data['room_type']][0],
-                    flat_info['views'], flat_info['total_square'], flat_info['live_square'],
-                    flat_info['kitchen_square'], flat_info['floor'], flat_info['total_floors'],
-                    flat_info['area'], flat_info['flat_number'], flat_info['street'], flat_info['district_main'],
-                    flat_info['district_local'], flat_info['description'].replace(';', ' ').replace('\n', ' '), 
-                    flat_info['flat_type'], flat_info['toilet'], flat_info['balcony'], flat_info['repair_status'], 
-                    flat_info['window_view'], flat_info['ceiling'], flat_info['house_type'], flat_info['house_year'], 
-                    flat_info['lifts'], flat_info['parking'], flat_info['gas']
-                ])
+                flat_hash = '{}{}{}{}{}{}'.format(
+                    flat_info['total_square'],
+                    offer_data['room_type'],
+                    flat_info['floor'],
+                    flat_info['district'],
+                    flat_info['microdistrict_main'],
+                    flat_info['street'],
+                    flat_info['flat_number']
+                )
+                if flat_hash in flats_hashes:
+                    print('[DOUBLE] Skip offer saving')
+                    continue
 
-        print('[I] Batch has been parsed and saved.')
+                flats_hashes.add(flat_hash)
+
+                with open(file_name, 'a', newline='', encoding='utf-8') as csv_file:
+                    csv_writer = csv.writer(csv_file, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    csv_writer.writerow([
+                        offer_id, offer_data['link'], flat_info['price'], ROOM_TYPES[offer_data['room_type']][0],
+                        flat_info['views'], flat_info['total_square'], flat_info['live_square'],
+                        flat_info['kitchen_square'], flat_info['floor'], flat_info['total_floors'],
+                        flat_info['residental_complex'], flat_info['district'], flat_info['flat_number'],
+                        flat_info['street'], flat_info['microdistrict_main'], flat_info['microdistrict_local'],
+                        flat_info['description'].replace(';', ' ').replace('\n', ' '), flat_info['flat_type'],
+                        flat_info['toilet'], flat_info['balcony'], flat_info['repair_status'], flat_info['window_view'],
+                        flat_info['ceiling'], flat_info['house_type'], flat_info['house_year'], flat_info['lifts'],
+                        flat_info['parking'], flat_info['gas']
+                    ])
+
+            print('[I] Batch has been parsed and saved.')
 
     timer_end = datetime.datetime.now()
     print('Scraping past in {} seconds. Fetched {} offers.'.format(
@@ -232,8 +242,9 @@ def main():
         flats_writer = csv.writer(flats_file, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         flats_writer.writerow([
             'ID', 'Link', 'Price', 'Rooms', 'Views', 'Square', 'Live Square', 'Kitchen', 'Floor', 'Total Floors',
-            'Area', 'Flat number', 'Street', 'District', 'District 2', 'Description', 'Flat Type', 'Toilet', 'Balcony',
-            'Repair', 'Window View', 'Ceiling', 'House Type', 'House Year', 'Lifts', 'Parking', 'Gas'
+            'Residental Complex', 'District', 'Flat number', 'Street', 'Microdistrict', 'Microistrict 2', 'Description',
+            'Flat Type', 'Toilet', 'Balcony', 'Repair', 'Window View', 'Ceiling', 'House Type', 'House Year', 'Lifts',
+            'Parking', 'Gas'
         ])
     init_parsing(file_name)
 
